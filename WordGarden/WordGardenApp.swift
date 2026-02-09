cb6d1ab1e4facccfe3ef094302bae56ccdc689af
Sankar Ram Subramanian <sankarram2002ybis@gmail.com>
2026-02-08 23:44:09 -0500

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Sankar Ram Subramanian on 8/2/26.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
